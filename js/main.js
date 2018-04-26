93cc3f19d6e70376756c38bf598778c9bc7b040d
saugat1998 <37047367+saugat1998@users.noreply.github.com>
2018-04-27 00:01:30 +0930

    navigation

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+document.getElementById("menu").addEventListener("click", openMenu);
+
+function openMenu() {
+
+  document.getElementById("dropdown").classList.toggle("active");
+}
+document.getElementById("slide").addEventListener("click", myfunction);
+
+function myfunction() {
+
+  document.getElementById("drop").classList.toggle("active");
+}
+document.getElementById("nav").addEventListener("click", my);
+
+function my() {
+
+
+  document.getElementById("down").classList.toggle("active");
+}
